120d6837cca97992ee95039873560b8b3e162672
GianlucaRossi8 <139228938+GianlucaRossi8@users.noreply.github.com>
2023-07-11 17:22:02 +0200

    Aggiunto WELCOME

--- ES_gioco_impiccato.py
+++ ES_gioco_impiccato.py
@@ -17,21 +17,27 @@ def replace_values(stringa, stringa_nascosta, char):
 
 
 # Scelta del livello: facciamo selezionare da utente sia la difficoltà delle parole che il numero di tentativi con cui
-# giocare
+# giocare. Implementiamo un while per gestire gli input non validi.
 livello = input("Scegli il livello di difficoltà fra 1, 2, 3:\n1->Facile\n2->Medio\n3->Difficile")
 while livello not in ['1', '2', '3']:
     print("Hai inserito un valore non valido. Scegli un livello valido.")
     livello = input("Scegli il livello di difficoltà fra 1, 2, 3:\n1->Facile\n2->Medio\n3->Difficile")
 
 livello_diff = {
-    "1": {"parole": ['ciao', 'bello', 'brutto', 'sole'], "n_tentativi": 8},
-    "2": {"parole": ['castello', 'lampadina', 'scaffale', 'forbice', 'habitat'], "n_tentativi": 7},
-    "3": {"parole": ["weekend", "bajour", "disarcivescovizzare"], "n_tentativi": 6}
+    "1": {"parole": ['ciao', 'bello', 'brutto', 'sole'], "n_tentativi": 8, "level": "EASY"},
+    "2": {"parole": ['castello', 'lampadina', 'scaffale', 'forbice', 'habitat'], "n_tentativi": 7, "level": "MEDIUM"},
+    "3": {"parole": ["weekend", "bajour", "disarcivescovizzare"], "n_tentativi": 6, "level": "HARD"}
 }
-
 
 lista_parole = livello_diff[livello]["parole"]
 n_tentativi = livello_diff[livello]["n_tentativi"]
+
+# Benvenuto
+print("===================================")
+print(f"BENVENUTO ALL'IMPICCATO!!!!")
+print(f"GIOCHERAI IN MODALITA' {livello_diff[livello]['level']}")
+print(f"HAI A DISPOSIZIONE {n_tentativi} tentativi")
+print("===================================\n")
 
 # Scegliamo una parola a caso fra quelle della lista_parole
 parola = random.choice(lista_parole)
